26391934e26d2f6ff2d2c9b9959d404e9c384608
xsimbvx <kirilljeee@gmail.com>
2015-12-10 03:23:31 +0200

    небольшие правки

--- Form1.cs
+++ Form1.cs
@@ -1,33 +1,85 @@
 ﻿using System;
 using System.Drawing;
 using System.Windows.Forms;
-using ImageRecovery.NonBlind;
 using ImageEditor;
 using ImageRecovery;
-using DeblurModification;
+using System.Threading;
 
 public partial class Form1 : Form
 {
     private Image sourceImage;                                      //исходное изображение
     private byte[,] noize = null;                                   //шум
     private ConvolutionFilter blur;                                 //искажающий
+    //потоки 
+    private Thread blurThread;                                      //для искажения изображения
+    private Thread noiseThread;                                     //для добавления шума
+    private Thread reconstructionThread;                            //для восстановления изображения
 
     /// <summary>
-    /// Конструкто
+    /// Изменения текста на форме из любого потока.
+    /// </summary>
+    /// <param name="label">Метка, текст которой надо изменить</param>
+    /// <param name="text">Новый текст</param>
+    private void ChangeText(Label label, string text)
+    {
+        if (label.InvokeRequired)
+        {
+            Action<Label, string> callback = ChangeText;
+            try
+            {
+                Invoke(callback, new object[] { label, text });
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+        else
+        {
+            label.Text = text;
+        }
+    }
+    /// <summary>
+    /// Изменения изображения на форме из любого потока.
+    /// </summary>
+    /// <param name="pictureBox">Picture box, изображение которого надо изменить.</param>
+    /// <param name="image">Новое изображение.</param>
+    private void ChangeImage(PictureBox pictureBox, Image image)
+    {
+        if (pictureBox.InvokeRequired)
+        {
+            Action<PictureBox, Image> callback = ChangeImage;
+            try
+            {
+                Invoke(callback, new object[] { pictureBox, image });
+            }
+            catch
+            {
+                // ignored
+            }
+        }
+        else
+        {
+            pictureBox.Image = image;
+        }
+    }
+
+    /// <summary>
+    /// Конструктор
     /// </summary>
     /// <param name="path">Путь к открываемому файлу</param>
     public Form1(string[] path)
     {
         try
         {
-            sourceImage = Image.FromFile(path[0]);                  //пытаемся грухить (может выскочить исключение)
-            InitializeComponent();                                  //инициализируем компоненты
-            sourcePictureBox.Image = sourceImage;                   //отрисуем избражение и укажим размеры
+            sourceImage = Image.FromFile(path[0]);                  //попытка грузить файл (может выскочить исключение)
+            InitializeComponent();                                  //инициализация компонентов
+            sourcePictureBox.Image = sourceImage;                   //отрисовка избражения и указывание размеров
             sourceSizeText.Text = sourcePictureBox.Image.Height + " x" + sourcePictureBox.Image.Width;
         }
         catch
         {
-            InitializeComponent();                                  //если поймали исключение - инициализируем компоненты
+            InitializeComponent();                                  //если поймано исключение - инициальзация компонентов
         }
     }
 
@@ -36,16 +88,16 @@ public partial class Form1 : Form
     /// </summary>
     private void LoadImage(object sender = null, EventArgs e = null)
     {
-        OpenFileDialog ofd = new OpenFileDialog();                  //открываем диалог
+        OpenFileDialog ofd = new OpenFileDialog();                  //открытие диалога
         if (ofd.ShowDialog() == DialogResult.OK)                    //если файл был выбран
         {
             try
             {
-                sourceImage = Image.FromFile(ofd.FileName);         //пытаемся его загрузить (может выскочить исключение)
-                sourcePictureBox.Image = sourceImage;               //если загружено - отрисуем и выведем размер
+                sourceImage = Image.FromFile(ofd.FileName);         //попытка его загрузить (может выскочить исключение)
+                sourcePictureBox.Image = sourceImage;               //если загружено - отрисовка и вывод размера
                 sourceSizeText.Text = sourcePictureBox.Image.Height + " x" + sourcePictureBox.Image.Width;
 
-                blurPictureBox.Image = null;                        //обнулим все следующие шаги обработки изображения
+                blurPictureBox.Image = null;                        //обнуление всех последующих шагов обработки изображения
                 noisePictureBox.Image = null;
                 recoveredPictureBox.Image = null;
 
@@ -53,7 +105,7 @@ public partial class Form1 : Form
                 noiseSizeText.Text = "-";
                 recoveredSizeText.Text = "-";
 
-                blurKernalPictureBox.Image = null;                  //и вспомогательные изображения
+                blurKernalPictureBox.Image = null;                  //и вспомогательные изображения (kernel)
                 noiseMaskPictureBox.Image = null;
                 recoveryKernalPictureBox.Image = null;
 
@@ -63,9 +115,7 @@ public partial class Form1 : Form
                 MessageBox.Show("Неверный формат изображения", "Ошибка загрузки");
                 sourceImage = sourcePictureBox.Image;               //вернём изображению предыдущее состояние
             }
-
         }
-
     }
 
     /// <summary>
@@ -73,12 +123,22 @@ public partial class Form1 : Form
     /// </summary>
     private void AddBlur(object sender = null, EventArgs e = null)
     {
+        if (blurThread != null)
+            blurThread.Abort();
         if (sourceImage == null)                                    //если входного изображения нет
         {
             LoadImage();                                            //грузим его
             if (sourceImage == null)                                //если его всё равно нет (пользователь отказался)
                 return;                                             //уходим
         }
+        //обнуляем следующие этапы
+        ChangeImage(noisePictureBox, null);
+        ChangeImage(recoveredPictureBox, null);
+        ChangeText(noiseSizeText, "-");
+        ChangeText(recoveredSizeText, "-");
+        ChangeImage(noiseMaskPictureBox, null);
+        ChangeImage(recoveryKernalPictureBox, null);
+
         if (sender == null)                                         //если функция была вызвана не кнопкой
         {
             blur = Filters.CopyFilter;                              //выставляем фильтр точной копии
@@ -95,19 +155,20 @@ public partial class Form1 : Form
                     blur = Filters.Gaussian5x5BlurFilter;
                     break;
             }
-            //проводим свёртку
-            blurPictureBox.Image = blur.Convolution(new Bitmap(sourceImage), ConvolutionFilter.ConvolutionMode.expand);
         }
-        //выведем размеры и ядро искажения
-        bluredSizeText.Text = blurPictureBox.Image.Height + " x" + blurPictureBox.Image.Width;
-        blurKernalPictureBox.Image = Converter.ToImage(Mull(blur.normalizedFilterMatrix, 255));
-        //обнуляем следующие итапы
-        noisePictureBox.Image = null;
-        recoveredPictureBox.Image = null;
-        noiseSizeText.Text = "-";
-        recoveredSizeText.Text = "-";
-        noiseMaskPictureBox.Image = null;
-        recoveryKernalPictureBox.Image = null;
+        blurThread = new Thread(() =>
+        {
+            ChangeImage(blurPictureBox, IRIntegration.Properties.Resources.Loading);
+            ChangeImage(blurKernalPictureBox, IRIntegration.Properties.Resources.Loading);
+            if (noiseThread != null) noiseThread.Abort();
+            if (reconstructionThread != null) reconstructionThread.Abort();
+            //проводим свёртку
+            ChangeImage(blurPictureBox, blur.Convolution(new Bitmap(sourceImage), ConvolutionFilter.ConvolutionMode.expand));
+            //выведем размеры и ядро искажения
+            ChangeText(bluredSizeText, blurPictureBox.Image.Height + " x" + blurPictureBox.Image.Width);
+            ChangeImage(blurKernalPictureBox, Converter.ToImage(Mull(blur.normalizedFilterMatrix, 255)));
+        });
+        blurThread.Start();
     }
 
     /// <summary>
@@ -129,23 +190,39 @@ public partial class Form1 : Form
     /// </summary>
     private void AddNoise(object sender = null, EventArgs e = null)
     {
-        if (blurPictureBox.Image == null)                           //если размытого изображения нет
+        if (noiseThread != null)
+            noiseThread.Abort();
+        if (blurThread != null)
+            if (blurThread.ThreadState == ThreadState.Running)
+            {
+                MessageBox.Show("Bluring not complete. Please, wait a little.");
+                return;
+            }
+        if (blurPictureBox.Image == null)
         {
-            AddBlur();                                              //пробуем его сделать
-            if (blurPictureBox.Image == null)                       //если не получилось
-                return;                                             //уходим
+            MessageBox.Show("At the firth you need to apply a blur.");
+            return;
         }
-        //применяем одиночный шум
-        noize = new byte[blurPictureBox.Image.Height, blurPictureBox.Image.Width];
-        noisePictureBox.Image = blurPictureBox.Image.SingleNoize(float.Parse(textBox1.Text), ref noize);
 
-        //вывлжим дополниетльную информацию
-        noiseSizeText.Text = noisePictureBox.Image.Height + " x" + noisePictureBox.Image.Width;
-        noiseMaskPictureBox.Image = Converter.ToImage(noize);
-        //обнуляем следующие итапы
-        recoveredPictureBox.Image = null;
-        recoveredSizeText.Text = "-";
-        recoveryKernalPictureBox.Image = null;
+        //обнуляем следующие этапы
+        ChangeImage(recoveredPictureBox, null);
+        ChangeText(recoveredSizeText, "-");
+        ChangeImage(recoveryKernalPictureBox, null);
+
+        noiseThread = new Thread(() =>
+        {
+            if (reconstructionThread != null) reconstructionThread.Abort();
+            ChangeImage(noisePictureBox, IRIntegration.Properties.Resources.Loading);
+            ChangeImage(noiseMaskPictureBox, IRIntegration.Properties.Resources.Loading);
+
+            //применяем одиночный шум
+            noize = new byte[blurPictureBox.Image.Height, blurPictureBox.Image.Width];
+            ChangeImage(noisePictureBox, blurPictureBox.Image.SingleNoize(float.Parse(textBox1.Text), ref noize));
+            //вывлжим дополниетльную информацию
+            ChangeText(noiseSizeText, noisePictureBox.Image.Height + " x" + noisePictureBox.Image.Width);
+            ChangeImage(noiseMaskPictureBox, Converter.ToImage(noize));
+        });
+        noiseThread.Start();
     }
 
     /// <summary>
@@ -173,68 +250,53 @@ public partial class Form1 : Form
     /// </summary>
     private void Recovery(object sender, EventArgs e)
     {
-        if (noisePictureBox.Image == null)                          //если зашумленного избражения нет
+        if (noisePictureBox.Image == null)
         {
-            AddNoise();                                             //пытаемся его создать
-            if (noisePictureBox.Image == null)                      //если не удалось
-                return;                                             //уходим
+            MessageBox.Show("At the firth you need to apply a noise.");
+            return;
         }
+        if (reconstructionThread != null)
+            reconstructionThread.Abort();
+        if (blurThread != null)
+            if (blurThread.ThreadState == ThreadState.Running)
+            {
+                MessageBox.Show("Bluring not complete. Please, wait a little.");
+                return;
+            }
+        if (noiseThread != null)
+            if (noiseThread.ThreadState == ThreadState.Running)
+            {
+                MessageBox.Show("Noising not complete. Please, wait a little.");
+                return;
+            }
+
         ConvolutionFilter filter = null;                            //обнуляем фильтр, возвращающий изображение в нормальный вид
-        double sourceMSDCBlur = sourceImage.MeanSquareDeviationComparator(blurPictureBox.Image);
-        double sourceGradientBlur = sourceImage.Comparate(blurPictureBox.Image);
-        switch (((Button)sender).Name)                              //выбираем алгоритм в зависимости от времени
+        reconstructionThread = new Thread(() =>
         {
-            case "InverseButton":
-                filter = blur;//InverseFiltering.Filtering(blur);
-                long start = System.DateTime.Now.Ticks;
-                Image newImage = InverseFiltering.Filtering(blurPictureBox.Image, blur, false);//filter.Convolution(new Bitmap(noisePictureBox.Image), ConvolutionFilter.ConvolutionMode.expand);
-                long time1 = System.DateTime.Now.Ticks - start;
-                int hashcode1 = newImage.GetHashCode();
+            ChangeImage(recoveredPictureBox, IRIntegration.Properties.Resources.Loading);
+            ChangeImage(recoveryKernalPictureBox, IRIntegration.Properties.Resources.Loading);
 
-                double sourceRecoveryMSDCWithoutInterpolate = sourceImage.MeanSquareDeviationComparator(newImage);
-                double sourceRecoveryGradientWithoutInterpolate = sourceImage.Comparate(newImage);
-                start = System.DateTime.Now.Ticks;
-                recoveredPictureBox.Image = InverseFiltering.Filtering(blurPictureBox.Image, blur, true);//filter.Convolution(new Bitmap(noisePictureBox.Image), ConvolutionFilter.ConvolutionMode.expand);
-                long time2 = System.DateTime.Now.Ticks - start;
-                double sourceRecoveryMSDCWithInterpolate = sourceImage.MeanSquareDeviationComparator(recoveredPictureBox.Image);
-                double sourceRecoveryGradientWithInterpolate = sourceImage.Comparate(recoveredPictureBox.Image);
+            switch (((Button)sender).Name)                              //выбираем алгоритм в зависимости от времени
+            {
+                case "InverseButton":
+                    filter = blur;
+                    recoveredPictureBox.Image = InverseFiltering.Filtering(noisePictureBox.Image, blur);//filter.Convolution(new Bitmap(noisePictureBox.Image), ConvolutionFilter.ConvolutionMode.expand);
+                    break;
 
-                string str = "СКО:\nБез восстановления: ";
-                str += sourceMSDCBlur;
-                str += "\nС восстановлением:\nБез интерполяции: ";
-                str += sourceRecoveryMSDCWithoutInterpolate;
-                str += "\nС интерполяцией: ";
-                str += sourceRecoveryMSDCWithInterpolate;
-                str += "\nКоэффициент: ";
-                str += sourceRecoveryMSDCWithoutInterpolate / sourceRecoveryMSDCWithInterpolate;
-                str += "\nГрадиент:\nБез восстановления: ";
-                str += sourceGradientBlur;
-                str += "\nС восстановлением:\nБез интерполяции: ";
-                str += sourceRecoveryGradientWithoutInterpolate;
-                str += "\nС интерполяцией: ";
-                str += sourceRecoveryGradientWithInterpolate;
-                str += "\nКоэффициент: ";
-                str += sourceRecoveryGradientWithoutInterpolate / sourceRecoveryGradientWithInterpolate;
-                MessageBox.Show(str);
+            }
+            //выводим дополнительную информацию
+            try
+            {
+                ChangeText(recoveredSizeText, "? x ?");
+            }
+            catch (InvalidOperationException)
+            {
+                ChangeText(recoveredSizeText, recoveredPictureBox.Image.Height + " x" + recoveredPictureBox.Image.Width);
+            }
+            ChangeImage(recoveryKernalPictureBox, Converter.ToImage(Mull(filter.normalizedFilterMatrix, 255)));
 
-
-                break;
-
-            case "WienerButton":
-                filter = WienerFiltering.Filtering(blur, sourceImage, noize);
-                //производим конволющию
-                recoveredPictureBox.Image = filter.Convolution(new Bitmap(noisePictureBox.Image), ConvolutionFilter.ConvolutionMode.expand);
-                break;
-            case "TikhonovButton":
-                filter = TikhonovFiltering.Filtering(blur);
-                //производим конволющию
-                recoveredPictureBox.Image = filter.Convolution(new Bitmap(noisePictureBox.Image), ConvolutionFilter.ConvolutionMode.expand);
-                break;
-        }
-        //выводим дополнительную информацию
-        recoveredSizeText.Text = recoveredPictureBox.Image.Height + " x" + recoveredPictureBox.Image.Width;
-        recoveryKernalPictureBox.Image = Converter.ToImage(Mull(filter.normalizedFilterMatrix, 255));
-
+        });
+        reconstructionThread.Start();
 
     }
 
